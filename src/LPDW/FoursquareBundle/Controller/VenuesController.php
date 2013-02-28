a9747eb0df36b20fc62ca06783fe5eea3adc90c3
BuZzi <franckgorin.01@gmail.com>
2013-02-28 15:23:40 +0100

    foursquare VenuesController

---
+++ src/LPDW/FoursquareBundle/Controller/VenuesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LPDW\FoursquareBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use LPDW\FoursquareBundle\Model\FoursquareApi;
+
+class VenuesController extends Controller
+{
+    /**
+     * @Route("/{station}/categories", name="lpdw_categories"")
+     * @Template()
+     */
+    public function categoriesAction($_oStation)
+    {
+        $client_key = $this->container->getParameter('client_key');
+        $client_secret = $this->container->getParameter('client_secret');
+
+        $foursquare = new FoursquareAPI($client_key,$client_secret);
+
+        return ;
+    }
+}
