d743de5877c6cae71c271045c023d386e458d1bf
BuZzi <franckgorin.01@gmail.com>
2013-04-17 15:43:36 +0200

    Add Twitter API Bundle

---
+++ src/LPDW/TwitterBundle/LPDWTwitterBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LPDW\TwitterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LPDWTwitterBundle extends Bundle
+{
+}
---
+++ src/LPDW/TwitterBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LPDW\TwitterBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
