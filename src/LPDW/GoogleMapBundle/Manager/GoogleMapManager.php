b114c1de2da0971c6620c8250f571f936e64e13f
Romain Ardiet <romain.ardiet@gmail.com>
2013-04-18 14:45:40 +0200

    Boutons retour + fix bug pour la hauteur de la carte

--- src/LPDW/GoogleMapBundle/Manager/GoogleMapManager.php
+++ src/LPDW/GoogleMapBundle/Manager/GoogleMapManager.php
@@ -54,7 +54,7 @@ class GoogleMapManager
         ));
 
         $_oMap->setStylesheetOptions(array(
-            'width' => '900px',
+            'width' => '100%',
             'height' => '600px'
         ));
 
